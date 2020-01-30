fa2594a6817d30de1852b2dae57e2283cd7defbd
itsumura-h <dumblepy@gmail.com>
2020-01-30 17:00:07 +0900

    update #15 add toHeader()

---
+++ src/basolato/header.nim
@@ -0,0 +1,31 @@
+import base, tables, json
+
+# Header: seq[tuple[key, value:string]]
+proc newHeaders*(i:int=0): Headers =
+  return newSeq[tuple[key, value:string]](i)
+
+
+# tuple => header
+proc toHeaders*(headersArg:openArray[tuple]): Headers =
+  var headers = newHeaders(headersArg.len)
+  for i, row in headersArg:
+    headers[i] = (row[0], row[1])
+  return headers
+
+# table => header
+proc toHeaders*(headersArg:Table): Headers =
+  var headers = newHeaders(headersArg.len)
+  var i = 0
+  for key, val in headersArg.pairs:
+    headers[i] = (key, val)
+    i.inc()
+  return headers
+
+# JsonNode => header
+proc toHeaders*(headersArg:JsonNode): Headers =
+  var headers = newHeaders(headersArg.len)
+  var i = 0
+  for key, val in headersArg.pairs:
+    headers[i] = (key, val.getStr)
+    i.inc()
+  return headers
