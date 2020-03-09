a3591be85d2f34f6d0d7b0206fe0fc7eb7f9b77a
itsumura-h <dumblepy@gmail.com>
2020-03-09 13:07:08 +0900

    complete test middleware

--- runTest.sh
+++ runTest.sh
@@ -8,5 +8,8 @@ nimble test
 killall main
 rm tests/server/main
 rm tests/server/db.sqlite3
+rm tests/server/session.db
+rm tests/server/session.db.bak
 rm -fr tests/server/logs/*
+
 find tests/ -type f ! -name "*.*" -delete 2>/dev/null
--- tests/test_middleware.nim
+++ tests/test_middleware.nim
@@ -23,7 +23,7 @@ suite "middleware":
       check getCurrentExceptionMsg() == "message2"
 
   test "checkCsrfToken":
-    client.headers = newHttpHeaders({ "Content-Type": "application/x-www-form-urlencoded"})
+    client.headers = newHttpHeaders({"Content-Type": "application/x-www-form-urlencoded"})
     let token = newCsrfToken("").getToken()
     echo token
     var params = &"csrf_token={token}"
@@ -31,9 +31,36 @@ suite "middleware":
     check response.code == Http200
 
   test "checkCsrfToken invalid":
-    client.headers = newHttpHeaders({ "Content-Type": "application/x-www-form-urlencoded"})
+    client.headers = newHttpHeaders({"Content-Type": "application/x-www-form-urlencoded"})
     var params = &"csrf_token=invalid_token"
     let response = client.post(&"{HOST}/with_middleware/test_routing", body = $params)
     # echo response.body
     check response.code == Http403
     check response.body.contains("Invalid csrf token")
+
+  test "checkAuthToken":
+    let auth_id = newAuth().getToken()
+    echo auth_id
+    client.headers = newHttpHeaders({
+      "Cookie": &"session_id={auth_id}",
+      "Content-Type": "application/x-www-form-urlencoded"
+    })
+    let csrf_token = newCsrfToken("").getToken()
+    var params = &"csrf_token={csrf_token}"
+    let response = client.post(&"{HOST}/with_middleware/test_routing", body = $params)
+    # echo response.body
+    check response.code == Http200
+
+  test "checkAuthToken invalid":
+    let auth_id = "invalid_auth_id".encryptCtr()
+    echo auth_id
+    client.headers = newHttpHeaders({
+      "Cookie": &"session_id={auth_id}",
+      "Content-Type": "application/x-www-form-urlencoded"
+    })
+    let csrf_token = newCsrfToken("").getToken()
+    var params = &"csrf_token={csrf_token}"
+    let response = client.post(&"{HOST}/with_middleware/test_routing", body = $params)
+    # echo response.body
+    check response.code == Http403
+    check response.body.contains("Invalid session id")
