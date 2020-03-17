f757c12e316dd7d4ac961e45e4ce3b5c93b806ec
itsumura-h <dumblepy@gmail.com>
2020-03-18 03:59:28 +0900

    wip

--- src/basolato/cli/functions/runServer.nim
+++ src/basolato/cli/functions/runServer.nim
@@ -1,18 +1,22 @@
-import os, tables, times, re, strformat, osproc, streams, terminal
+import
+  os, tables, times, re, strformat, osproc, streams, options, asyncdispatch,
+  asyncstreams
 
 let
-  sleepTime = 2
+  sleepTime = 1
   currentDir = getCurrentDir()
   cmds = [
     "killall main",
     "nim c main.nim",
     "./main &"
   ]
+
 var
   files: Table[string, Time]
   isModified = false
   p: Process
   pid = 0
+  # outp: Stream
 
 proc ctrlC() {.noconv.} =
   close(p)
@@ -21,37 +25,45 @@ proc ctrlC() {.noconv.} =
   quit 0
 setControlCHook(ctrlC)
 
-proc display() =
-  var outp = outputStream(p)
-  outp = StringStream(outp)
-  close inputStream(p)
-  var line = newStringOfCap(120).TaintedString
-  while true:
-    echo outp.readAll().repr
-    discard outp.readLine(line)
-    echo line
+# proc display() {.async.} =
+#   echo "=== display"
+#   var line = newStringOfCap(120).TaintedString
+#   var outp = newFutureStream[string]("outputStream")
+#   while not outp.finished():
+#     echo "=== while"
+#     var (hasValue, value) = await outp.read()
+#     for line in value.lines():
+#       echo line
+#     break
+
     
 
-proc runCommand() =
+proc runCommand() {.async.} =
   if pid > 0:
     discard execShellCmd(&"kill {pid}")
   discard execShellCmd("nim c main")
   try:
-    p = startProcess("./main", currentDir, ["&"])
+    p = startProcess("./main", currentDir, ["&"],
+                    options={poInteractive})
     pid = p.processID()
-    echo "=== modified display"
-    display()
+    # outp = outputStream(p)
+    discard outputStream(p)
+    var outp = newFutureStream[string]("outputStream")
+    outp.callback=(proc(future:FutureStream[string])=
+      var (_, value) = future.read()
+      echo value
+    )
   except:
     echo getCurrentExceptionMsg()
 
 proc serve*() =
-  runCommand()
+  discard runCommand()
   while true:
     sleep sleepTime * 1000
     for f in walkDirRec(currentDir, {pcFile}):
       if f.find(re"\.nim$") > -1:
         let modTime = getFileInfo(f).lastWriteTime
-        display()
+        # discard display()
         if not files.hasKey(f):
           files[f] = modTime
           # debugEcho &"Skip {f} because of first checking"
@@ -65,4 +77,4 @@ proc serve*() =
       
     if isModified:
       isModified = false
-      runCommand()
+      discard runCommand()
