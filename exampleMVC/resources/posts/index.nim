5209652fac7eb43a44d52a9453b6ce0d9e65f1f1
itsumura-h <dumblepy@gmail.com>
2020-03-19 20:21:11 +0900

    close #40
    complete remove karax and replace templates

--- exampleMVC/resources/posts/index.nim
+++ exampleMVC/resources/posts/index.nim
@@ -1,19 +1,19 @@
-import json, strformat
-# 3rd party
-import karax / [karaxdsl, vdom]
+import json
 # framework
 import ../../../src/basolato/view
 import ../base
-proc indexHtmlImpl(posts:seq[JsonNode]):string =
-  var vnode = buildHtml(tdiv):
-    for post in posts:
-      tdiv(class="post"):
-        tdiv(class="date"):
-          p: text(&"""published: {post["published_date"].get}""")
-        h2:
-          a(href= &"""/posts/{post["id"].get}"""): text(post["title"].get)
-        p: text(post["text"].get)
-  return $vnode
+
+proc indexHtmlImpl(posts:seq[JsonNode]):string = tmpli html"""
+$for post in posts {
+  <div class="post">
+      <div class="date">
+        <p>published: $(post["published_date"].get)</p>
+      </div>
+    <h2><a href="/posts/$(post["id"].get)">$(post["title"].get)</a></h2>
+    <p>$(post["text"].get)</p>
+  </div>
+}
+"""
 
 proc indexHtml*(auth:Auth, posts:seq[JsonNode]): string =
   baseHtml(auth, indexHtmlImpl(posts))
