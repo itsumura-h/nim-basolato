dca5d13156f356086322f7ba41c51b3386e18412
itsumura-h <dumblepy@gmail.com>
2020-04-10 08:46:00 +0900

    wip

--- rails_tutorial/tests/test_user_sign_up.nim
+++ rails_tutorial/tests/test_user_sign_up.nim
@@ -15,33 +15,22 @@ suite "form sign up":
   setup:
     var client = newHttpClient()
 
-  test "a":
-    # client.headers = newHttpHeaders({"Content-Type": "application/x-www-form-urlencoded"})
-    # var data = %*{
-    #   "csrf_token": newCsrfToken().getToken(),
-    #   "name": "",
-    #   "email": "user@invalid",
-    #   "password": "foo",
-    #   "password_confirm": "bar"
-    # }
-    # var response = client.post(&"{HOST}/users", body= $data)
-
-    # var data = newMultipartData()
-    # data["csrf_token"] = newCsrfToken().getToken()
-    # data["name"] = ""
-    # data["email"] = "user@invalid"
-    # data["password"] = "foo"
-    # data["password_confirm"] = "bar"
-    let data = @[
-      ("csrf_token", newCsrfToken().getToken()),
-      ("name", ""),
-      ("email", "user@invalid"),
-      ("password", "foo"),
-      ("password_confirm", "bar"),
-    ]
-    # var response = client.post(&"{HOST}/users", multipart=data)
-    echo data.toBody()
-    var response = client.post(&"{HOST}/users", body=data.toBody())
-
-    echo response.status
-    echo response.body
+  test "success":
+    client.headers = newHttpHeaders({"Content-Type": "application/x-www-form-urlencoded"})
+    # let data = @[
+    #   ("csrf_token", newCsrfToken().getToken()),
+    #   ("name", ""),
+    #   ("email", "user@invalid"),
+    #   ("password", "foo"),
+    #   ("password_confirm", "bar"),
+    # ]
+    # echo data.toBody()
+    var data = newMultipartData()
+    data["csrf_token"] = newCsrfToken().getToken()
+    data["name"] = ""
+    data["email"] = "user@invalid"
+    data["password"] = "foo"
+    data["password_confirm"] = "bar"
+    echo $data
+    # var response = client.post(&"{HOST}/users", body=data.toBody())
+    # check response.
