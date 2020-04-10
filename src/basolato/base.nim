d763104e55e2f62cafe6f29a2ec324ccbe9ca858
itsumura-h <dumblepy@gmail.com>
2020-04-10 11:06:40 +0900

    merge

--- src/basolato/base.nim
+++ src/basolato/base.nim
@@ -1,19 +1,7 @@
 import json, httpcore
 
-<<<<<<< HEAD
-const basolatoVersion* = "0.2.9"
-=======
 const
   basolatoVersion* = "0.3.0"
-  IS_DISPLAY* = getEnv("LOG_IS_DISPLAY").string.parseBool
-  IS_FILE* = getEnv("LOG_IS_FILE").string.parseBool
-  LOG_DIR* = getEnv("LOG_DIR").string
-  SECRET_KEY* = getEnv("SECRET_KEY").string
-  CSRF_TIME* = getEnv("CSRF_TIME").string.parseInt
-  SESSION_TIME* = getEnv("SESSION_TIME").string.parseInt
-  SESSION_DB_PATH* = getEnv("SESSION_DB_PATH").string
-  IS_SESSION_MEMORY* = getEnv("IS_SESSION_MEMORY").string.parseBool
->>>>>>> 35c9b4be8b52d1c0c80d8e106b871097050cbdb2
 
 type
   Response* = ref object
