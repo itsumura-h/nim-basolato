f11b9c33bb2eadfc494a10889c999575693c1a7a
itsumura-h <dumblepy@gmail.com>
2020-04-10 10:50:09 +0900

    wip

--- rails_tutorial/tests/test_user_sign_up.nim
+++ rails_tutorial/tests/test_user_sign_up.nim
@@ -1,36 +1,23 @@
-import unittest, strformat, httpclient, strutils, json
+import unittest, strformat, httpclient, strutils
 import basolato/security
+import basolato/test/helper
 
 const HOST = "http://0.0.0.0:5000"
-
-proc toBody(body:openarray[tuple[key, value:string]]):string =
-  result = ""
-  for row in body:
-    if result.len == 0:
-      result.add(&"{row.key}={row.value}")
-    else:
-      result.add(&"&{row.key}={row.value}")
 
 suite "form sign up":
   setup:
     var client = newHttpClient()
 
   test "success":
-    client.headers = newHttpHeaders({"Content-Type": "application/x-www-form-urlencoded"})
-    # let data = @[
-    #   ("csrf_token", newCsrfToken().getToken()),
-    #   ("name", ""),
-    #   ("email", "user@invalid"),
-    #   ("password", "foo"),
-    #   ("password_confirm", "bar"),
-    # ]
-    # echo data.toBody()
-    var data = newMultipartData()
-    data["csrf_token"] = newCsrfToken().getToken()
-    data["name"] = ""
-    data["email"] = "user@invalid"
-    data["password"] = "foo"
-    data["password_confirm"] = "bar"
-    echo $data
-    # var response = client.post(&"{HOST}/users", body=data.toBody())
-    # check response.
+    let data = @[
+      ("csrf_token", newCsrfToken().getToken()),
+      ("name", ""),
+      ("email", "user@invalid"),
+      ("password", "foo"),
+      ("password_confirm", "bar"),
+    ]
+    var response = client.formpost(&"{HOST}/users", data)
+    echo response.status
+    echo response.body
+    check response.status == Http500
+    check response.body.contains("password is not match")
