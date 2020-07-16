bb00052640ba6598da958729a3e5eab1857c6f01
itsumura-h <dumblepy@gmail.com>
2020-07-16 21:54:35 +0900

    delete tests/server2

--- tests/server2/middleware.nim
+++
@@ -1,5 +0,0 @@
-import ../../src/basolato/middleware
-
-template framework*() =
-  checkCsrfToken(request).catch(Error403)
-  checkAuthToken(request).catch(Error403)
--- tests/server2/resources/test_template.nim
+++
@@ -1,3 +0,0 @@
-#? stdtmpl | standard
-#proc test_template*():string =
-<h1>test template</h1>
