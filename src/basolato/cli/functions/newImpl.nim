f2312a491827a328ebfc0246afadccae1894b513
itsumura-h <dumblepy@gmail.com>
2020-07-22 18:25:02 +0900

    update doc

--- src/basolato/cli/functions/newImpl.nim
+++ src/basolato/cli/functions/newImpl.nim
@@ -45,7 +45,7 @@ proc create(dirPath:string, packageDir:string):int =
 
 version       = "0.1.0"
 author        = "Anonymous"
-description   = "A new awesome baspolato package"
+description   = "A new awesome basolato package"
 license       = "MIT"
 srcDir        = "."
 bin           = @["main"]
@@ -61,7 +61,7 @@ requires "cligen >= 0.9.41"
 requires "templates >= 0.5"
 requires "bcrypt >= 0.2.1"
 requires "nimAES >= 0.1.2"
-requires "https://github.com/enthus1ast/flatdb >= 0.2.4"
+requires "flatdb >= 0.2.4"
 requires "allographer >= 0.9.0"
 requires "faker >= 0.12.1"
 """
