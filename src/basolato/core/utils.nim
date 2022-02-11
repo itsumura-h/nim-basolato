61c59dee27e5bd8ec9b61373759704a7d23745c1
itsumura-h <dumblepy@gmail.com>
2022-02-12 01:58:20 +0900

    wip

--- src/basolato/core/utils.nim
+++ src/basolato/core/utils.nim
@@ -17,12 +17,17 @@ func makeCookie*(key, value, expires: string, domain = "", path = "",
   if sameSite != None:
     result.add("; SameSite=" & $sameSite)
 
-func getOsName():string =
-  const f = staticRead("/etc/os-release")
-  for row in f.split("\n"):
-    let kv = row.split("=")
-    if kv[0] == "ID":
-      return kv[1]
+func getOsName*():string =
+  when defined(maxosx) or defined(linux) or defined(bsd):
+    const f = staticRead("/etc/os-release")
+    for row in f.split("\n"):
+      let kv = row.split("=")
+      if kv[0] == "ID":
+        return kv[1]
+  elif defined(windows):
+    return "windows"
+  else:
+    return ""
 
 proc isExistsLibsass*():bool =
   ## used in /view
@@ -42,9 +47,10 @@ proc isExistsLibsass*():bool =
       return res.exitCode == 0 and res.output.len > 0
     else:
       raise newException(Exception, "The only supported OS of Linux are Alpine, Debian, and Ubuntu.")
-  else: # Windows
-    const libDir = "/usr/lib /usr/local/lib"
-    const libsass = "libsass.dll"
-    const query = "find " & libDir & " -name \"" & libsass & "\""
-    const res = gorgeEx(query)
-    return res.exitCode == 0 and res.output.len > 0
+  elif defined(windows):
+    # const libDir = "/usr/lib /usr/local/lib"
+    # const libsass = "libsass.dll"
+    # const query = "find " & libDir & " -name \"" & libsass & "\""
+    # const res = gorgeEx(query)
+    # return res.exitCode == 0 and res.output.len > 0
+    return true
