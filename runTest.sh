173fb910dec9e966b781f29ddd6fa20b8e3b0416
itsumura-h <dumblepy@gmail.com>
2022-07-13 17:55:36 +0900

    wip

--- runTest.sh
+++ runTest.sh
@@ -1,20 +1,3 @@
-# nimble install -y
-# cd /root/project/tests/server
-# nim c main
-# nohup ./main > /dev/null 2>&1 &
-# cd /root/project/
-# touch tests/server/session.db
-# nimble test
-
-# killall main
-# rm tests/server/main
-# rm tests/server/db.sqlite3
-# rm tests/server/session.db
-# rm tests/server/session.db.bak
-# rm -fr tests/server/logs/*
-
-# find tests/ -type f ! -name "*.*" -delete 2>/dev/null
-
 set -eux
 
 nim -v
@@ -35,7 +18,7 @@ rm -fr ./testresults
 testament p "tests/test_*.nim"
 
 # delete files
-killall main
+pkill main
 rm tests/server/main
 rm tests/server/db.sqlite3
 rm tests/server/session.db
