5747cda230ed5aa4e051292ae4eb3493a7f46187
itsumura-h <dumblepy@gmail.com>
2022-07-14 01:12:23 +0900

    set defualt value in env

--- src/basolato/core/baseEnv.nim
+++ src/basolato/core/baseEnv.nim
@@ -1,7 +1,7 @@
 import os, strutils, streams, parsecfg
 
 const
-  SESSION_TYPE* = getEnv("SESSION_TYPE")
+  SESSION_TYPE* = getEnv("SESSION_TYPE", "file")
   DOES_USE_LIBSASS* = when existsEnv("LIBSASS"): getEnv("LIBSASS").parseBool else: false
 
 for f in walkDir(getCurrentDir()):
@@ -23,16 +23,16 @@ let
   SECRET_KEY* = getEnv("SECRET_KEY")
   PORT_NUM* = getEnv("PORT", "5000").parseInt
   # Logging
-  IS_DISPLAY* = getEnv("LOG_IS_DISPLAY").parseBool
-  IS_FILE* = getEnv("LOG_IS_FILE").parseBool
-  IS_ERROR_FILE* = getEnv("LOG_IS_ERROR_FILE").parseBool
-  LOG_DIR* = getEnv("LOG_DIR")
+  IS_DISPLAY* = getEnv("LOG_IS_DISPLAY", $true).parseBool
+  IS_FILE* = getEnv("LOG_IS_FILE", $true).parseBool
+  IS_ERROR_FILE* = getEnv("LOG_IS_ERROR_FILE", $true).parseBool
+  LOG_DIR* = getEnv("LOG_DIR", getCurrentDir() / "logs")
   # Session db
-  SESSION_DB_PATH* = getEnv("SESSION_DB_PATH")
-  SESSION_TIME* = getEnv("SESSION_TIME").parseInt
+  SESSION_DB_PATH* = getEnv("SESSION_DB_PATH", getCurrentDir() / "session.db")
+  SESSION_TIME* = getEnv("SESSION_TIME", "20160").parseInt
   COOKIE_DOMAINS* = getEnv("COOKIE_DOMAINS")
-  ENABLE_ANONYMOUS_COOKIE* = getEnv("ENABLE_ANONYMOUS_COOKIE").parseBool
+  ENABLE_ANONYMOUS_COOKIE* = getEnv("ENABLE_ANONYMOUS_COOKIE", $true).parseBool
 
   # others
   HOST_ADDR* = getEnv("HOST", "0.0.0.0")
-  LOCALE* = getEnv("LOCALE")
+  LOCALE* = getEnv("LOCALE", "en")
