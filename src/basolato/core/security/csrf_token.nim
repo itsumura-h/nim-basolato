64f6a486c547e8d4801cbfa2b65312e3d3e78017
itsumura-h <dumblepy@gmail.com>
2023-11-03 09:00:26 +0000

    delete echo

--- src/basolato/core/security/csrf_token.nim
+++ src/basolato/core/security/csrf_token.nim
@@ -16,10 +16,6 @@ func getToken*(self:CsrfToken):string =
   self.token
 
 proc checkCsrfValid*(self:CsrfToken, session:Option[Session]):Future[bool] {.async.} =
-  echo "=== checkCsrfValid"
-  echo "self.token: ",self.token
-  echo "session: ",session.repr
-  echo "session.get(\"nonce\").await: ",session.get("nonce").await
   if not session.isSome:
     return false
   let nonce = session.get("nonce").await
