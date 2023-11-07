ff14cd89aed0603752b163d13a6a961d6d753a50
itsumura-h <dumblepy@gmail.com>
2023-11-07 12:18:39 +0000

    fix

--- runTest.sh
+++ runTest.sh
@@ -5,26 +5,24 @@ nimble -v
 
 # run server
 nimble install -y
-cd /root/project/tests/server
-nim c --mm:orc --threads:off -d:ssl --parallelBuild:0 --putenv:PORT=5000 main
-echo "=================="
-nohup ./main > /dev/null 2>&1 &
-echo "=================="
+# cd /root/project/tests/server
+# nim c --mm:orc --threads:off -d:ssl --parallelBuild:0 --putenv:PORT=5000 main
+# nohup ./main > /dev/null 2>&1 &
 
 # run test
 cd /root/project/
-touch tests/server/session.db
-touch tests/server/db.sqlite3
-cp tests/server/.env ./
-rm -fr ./testresults
-testament p "tests/test_*.nim"
+# touch tests/server/session.db
+# touch tests/server/db.sqlite3
+# cp tests/server/.env ./
+# rm -fr ./testresults
+# testament p "tests/test_*.nim"
+# pkill main
 testament p "tests/*/test_*.nim"
 
 # delete files
-pkill main
-rm tests/server/main
-rm tests/server/db.sqlite3
-rm tests/server/session.db
-rm -fr tests/server/logs/*
-rm .env
-find tests/ -type f ! -name "*.*" -delete 2> /dev/null
+# rm tests/server/main
+# rm tests/server/db.sqlite3
+# rm tests/server/session.db
+# rm -fr tests/server/logs/*
+# rm .env
+# find tests/ -type f ! -name "*.*" -delete 2> /dev/null
