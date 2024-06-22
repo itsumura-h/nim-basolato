71da6f0bacc870e8c398b4f6dbd8a8757528d3d8
itsumura-h <dumblepy@gmail.com>
2024-06-22 10:24:23 +0000

    rm minimal sample

--- examples/minimam_example/app/controllers/example_controller.nim
+++
@@ -1,8 +0,0 @@
-import ../../../../src/basolato/controller
-
-
-proc index*(context:Context, params:Params):Future[Response] {.async.} =
-  return render("index")
-
-proc show*(context:Context, params:Params):Future[Response] {.async.} =
-  return render("show")
