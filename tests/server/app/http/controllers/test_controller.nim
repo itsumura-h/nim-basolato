6cf74aa5180e85653f31d10823afe7d67b0e5fcb
itsumura-h <dumblepy@gmail.com>
2024-07-20 06:37:30 +0000

    fix test

--- tests/server/app/http/controllers/test_controller.nim
+++ tests/server/app/http/controllers/test_controller.nim
@@ -7,42 +7,42 @@ import ../views/pages/test_view
 
 
 # test controller
-proc renderStr*(context:Context, params:Params):Future[Response] {.async.} =
+proc renderStr*(context:Context):Future[Response] {.async.} =
   return render("test")
 
-proc renderHtml*(context:Context, params:Params):Future[Response] {.async.} =
+proc renderHtml*(context:Context):Future[Response] {.async.} =
   return render(html("pages/test.html"))
 
-proc renderTemplate*(context:Context, params:Params):Future[Response] {.async.} =
+proc renderTemplate*(context:Context):Future[Response] {.async.} =
   return render(testView())
 
-proc renderJson*(context:Context, params:Params):Future[Response] {.async.} =
+proc renderJson*(context:Context):Future[Response] {.async.} =
   return render(%*{"key": "test"})
 
-proc status500*(context:Context, params:Params):Future[Response] {.async.} =
+proc status500*(context:Context):Future[Response] {.async.} =
   return render(Http500, "")
 
-proc status500json*(context:Context, params:Params):Future[Response] {.async.} =
+proc status500json*(context:Context):Future[Response] {.async.} =
   return render(Http500, %*{"key": "test"})
 
-proc redirect*(context:Context, params:Params):Future[Response] {.async.} =
+proc redirect*(context:Context):Future[Response] {.async.} =
   return redirect("/new_url")
 
-proc redirectWithHeader*(context:Context, params:Params):Future[Response] {.async.} =
+proc redirectWithHeader*(context:Context):Future[Response] {.async.} =
   let headers = newHttpHeaders()
   headers["key"] = "value"
   return redirect("/new_url", headers)
 
-proc errorRedirect*(context:Context, params:Params):Future[Response] {.async.} =
+proc errorRedirect*(context:Context):Future[Response] {.async.} =
   return errorRedirect("/new_url")
 
-proc errorRedirectWithHeader*(context:Context, params:Params):Future[Response] {.async.} =
+proc errorRedirectWithHeader*(context:Context):Future[Response] {.async.} =
   let headers = newHttpHeaders()
   headers["key"] = "value"
   return errorRedirect("/new_url", headers)
 
 # test helper
-proc dd*(context:Context, params:Params):Future[Response] {.async.} =
+proc dd*(context:Context):Future[Response] {.async.} =
   var a = %*{
     "key1": "value1",
     "key2": 2
@@ -51,48 +51,48 @@ proc dd*(context:Context, params:Params):Future[Response] {.async.} =
   return render("dd")
 
 # test response
-proc setHeader*(context:Context, params:Params):Future[Response] {.async.} =
+proc setHeader*(context:Context):Future[Response] {.async.} =
   var header = newHttpHeaders()
   header.add("key1", "value1")
   header.add("key2", ["value1", "value2"])
   return render("setHeader", header)
 
-proc setCookie*(context:Context, params:Params):Future[Response] {.async.} =
+proc setCookie*(context:Context):Future[Response] {.async.} =
   var cookies = Cookies.new(context.request)
   cookies.set("key1", "value1")
   cookies.set("key2", "value2")
   return render("setCookie").setCookie(cookies)
 
-proc setAuth*(context:Context, params:Params):Future[Response] {.async.} =
+proc setAuth*(context:Context):Future[Response] {.async.} =
   await context.set("key1", "value1")
   await context.set("key2", "value2")
   return render("setAuth")
 
-proc destroyAuth*(context:Context, params:Params):Future[Response] {.async.} =
+proc destroyAuth*(context:Context):Future[Response] {.async.} =
   await context.login()
   let res = await render("setAuth").destroyContext(context)
   return res
 
 
 # test routing
-proc getAction*(context:Context, params:Params):Future[Response] {.async.} =
+proc getAction*(context:Context):Future[Response] {.async.} =
   return render("get")
 
-proc postAction*(context:Context, params:Params):Future[Response] {.async.} =
-  let status = params.getStr("status")
+proc postAction*(context:Context):Future[Response] {.async.} =
+  let status = context.params.getStr("status")
   if status == "invalid":
     return render("invalid status")
   return render("post")
 
-proc patchAction*(context:Context, params:Params):Future[Response] {.async.} =
+proc patchAction*(context:Context):Future[Response] {.async.} =
   return render("patch")
 
-proc putAction*(context:Context, params:Params):Future[Response] {.async.} =
+proc putAction*(context:Context):Future[Response] {.async.} =
   return render("put")
 
-proc deleteAction*(context:Context, params:Params):Future[Response] {.async.} =
+proc deleteAction*(context:Context):Future[Response] {.async.} =
   return render("delete")
 
 # csrf token
-proc getCsrf*(context:Context, params:Params):Future[Response] {.async.} =
+proc getCsrf*(context:Context):Future[Response] {.async.} =
   return render(testView())
--- tests/test_signal.nim
+++ tests/test_signal.nim
@@ -10,19 +10,19 @@ suite("test signal"):
   test("crate signal"):
     let userSignal = createSignal((isLogin:true, name: "John", email: "john@example.com"))
     let user = userSignal.value()
-    check user.isLogin = true
+    check user.isLogin == true
     check user.name == "John"
     check user.email == "john@example.com"
 
   test("set signal"):
     let userSignal = createSignal((isLogin:false, name: "", email: ""))
-    let user = userSignal.value()
+    var user = userSignal.value()
     check user.isLogin == false
     check user.name == ""
     check user.email == ""
     
     userSignal.value = (isLogin:true, name: "John", email: "john@example.com")
-    let user = userSignal.value()
+    user = userSignal.value()
     check user.isLogin == true
     check user.name == "John"
     check user.email == "john@example.com"
